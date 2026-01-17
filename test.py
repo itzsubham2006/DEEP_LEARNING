c234ea70d40d1542e295d2d2727f639c67dddfff
subham <lastw5232@gmail.com>
2026-01-17 15:50:44 +0000

    NLP Introduce

---
+++ test.py
@@ -0,0 +1,14 @@
+docs = [
+    'hi my name is subham',
+    'hi this is me',
+    'this is a text message'
+]
+
+class students():
+    
+    def sum(self, a, b):
+        return a+b
+    
+    def study(self):
+        return f'Good at studies.'
+    
