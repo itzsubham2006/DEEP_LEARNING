186c4efa16b99a56bd229c5a53db6928552c387e
subham <lastw5232@gmail.com>
2026-02-04 13:31:24 +0000

    word2vec

--- test.py
+++ test.py
@@ -1,14 +1,35 @@
-docs = [
-    'hi my name is subham',
-    'hi this is me',
-    'this is a text message'
-]
+class students:
+    
+    def __init__(self, age, college_name, hostel_name):
+        self.age = age
+        self.college_name = college_name
+        self.hostel_name = hostel_name
+        self.menu()
+    
+    def goodmorning(self, name):
+        print(f"HEllo good morning {name}")
+        
+    
+    def sum(self, a):
+        count = 0
+        for i in a:
+            count = count+i
+        print(count)
+        return count
+    
+    
+    def students_info(self):
+        print(f"Students info: {self.age}, {self.college_name}, {self.hostel_name}.")
+    
+    def menu(self):
+        print("Object created successfully!!..")
+    
+    
+a = students(age=23, college_name='CITK', hostel_name='SJ')
 
-class students():
+a.students_info()
+
+
+
     
-    def sum(self, a, b):
-        return a+b
-    
-    def study(self):
-        return f'Good at studies.'
-    
+
